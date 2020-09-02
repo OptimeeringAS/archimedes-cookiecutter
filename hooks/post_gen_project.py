d2256892b9c90c584d92edd8ad26947cc51749e5
Jo Vederhus <jo.vederhus@gmail.com>
2020-09-02 11:33:14 +0200

    update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
-from cookiecutter.main import cookiecutter
+# from cookiecutter.main import cookiecutter
 
-create_examples = '{{cookiecutter.examples}}' == 'yes'
+# create_examples = '{{cookiecutter.examples}}' == 'yes'
 
-if create_examples:
-  cookiecutter('https://git.optimeering.no/archimedes/archimedes-cookiecutter-examples.git',
-             no_input=True,
-             extra_context={'repo_name': 'production'})
+# if create_examples:
+#   cookiecutter('https://git.optimeering.no/archimedes/archimedes-cookiecutter-examples.git',
+#              no_input=True,
+#              extra_context={'repo_name': 'production'})
