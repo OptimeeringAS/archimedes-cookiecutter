37600769aa37a0ab6a08242610bb827b07ee8d88
Pravin Dahal <pravin.dahal@optimeering.com>
2022-01-26 10:32:34 +0100

    Replace Gitlab links with corresponding GitHub links

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,6 @@
 # create_examples = '{{cookiecutter.examples}}' == 'yes'
 
 # if create_examples:
-#   cookiecutter('https://git.optimeering.no/archimedes/archimedes-cookiecutter-examples.git',
+#   cookiecutter('https://github.com/OptimeeringAS/archimedes-cookiecutter-examples.git',
 #              no_input=True,
 #              extra_context={'repo_name': 'production'})
